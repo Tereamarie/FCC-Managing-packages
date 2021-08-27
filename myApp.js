2e3c20fe36c90aba6cd241135ef85fe8fa851d3e
teresa knowlton <mtknowlton67@gmail.com>
2021-08-27 16:35:57 -0700

    Console.log

--- myApp.js
+++ myApp.js
@@ -1,27 +1,28 @@
 var express = require('express');
 var bGround = require('fcc-express-bground');
 var app = express();
-// --> 7) Mount the logger middleware here
-// --> 11) Mount the body-parser middleware here
+// // --> 7) Mount the logger middleware here
+// // --> 11) Mount the body-parser middleware here
 
 
-/** 1) Meet the node console. */
-  console.log("Hello World");
-/** 2) A first working Express Server */
+// /** 1) Meet the node console. */
+//   console.log("Hello World");
+// /** 2) A first working Express Server */
 
-/** 3) Serve an HTML file */
+// /** 3) Serve an HTML file */
 
-/** 4) Serve static assets */
+// /** 4) Serve static assets */
 
-/** 5) serve JSON on a specific route */
+// /** 5) serve JSON on a specific route */
 
-/** 6) Use the .env file to configure the app */
+// /** 6) Use the .env file to configure the app */
 
-/** 7) Root-level Middleware - A logger */
-// place it before all the routes !
+// /** 7) Root-level Middleware - A logger */
+// // place it before all the routes !
 
-/** 8) Chaining middleware. A time server */
+// /** 8) Chaining middleware. A time server */
 
+console.log("Hello world");
 
 
 
